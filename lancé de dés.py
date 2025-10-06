9ecb18836fd87b34bd705c9133e5cb782b1327df
Yannickthomas0602 <yannick.thomas@supdevinci-edu.fr>
2025-10-06 15:35:14 +0200

    Create lancé de dés.py

---
+++ lancé de dés.py
@@ -0,0 +1,16 @@
+import random
+
+def lancer_de():
+    """Simule un lancer de dé et renvoie le résultat."""
+    de = random.randint(1, 6)
+    print(f"Le dé affiche : {de}")
+
+    if de == 6:
+        print("🎉 Gagné !")
+    elif de == 1:
+        print("😢 Perdu...")
+    else:
+        print("Pas de gain, réessaie !")
+
+# Exemple d'appel de la fonction
+lancer_de()
