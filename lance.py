660403dbc6293b05756bc3b00a347cdfb9ffadfa
timeo-girard <timeo.girard@supdevinci-edu.fr>
2025-10-06 15:57:12 +0200

    Update lance.py
    force

--- lance.py
+++ lance.py
@@ -1,19 +1,1 @@
-import random
 
-def lancer_de():
-    """Simule un lancer de dé et renvoie le résultat."""
-    de = random.randint(1, 6)
-    print(f"Le dé affiche : {de}")
-
-    if de == 6:
-        print("\(°o°)/ Gagné !")
-    elif de == 1:
-        print("Perdu...")
-        print("🎉 Gagné !")
-    elif de == 1:
-        print("😢 Perdu...")
-    else:
-        print("Pas de gain, réessaie !")
-
-# Exemple d'appel de la fonction
-lancer_de()
